fdbae1679e0b111db34c208dedf6f344e79efffa
Yogesh Mishra <120166125+Yogi-1996@users.noreply.github.com>
2026-01-04 23:29:29 +0530

    Update hash.go
    error password cannot be empty added in hash

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -5,15 +5,22 @@ import (
 )
 
 func GenerateHash(password string) (string, error) {
+	if password == "" {
+		return "", errors.New("password cannot be empty")
+	}
+	
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	if err != nil {
 		return "", err
 	}
-
+	
 	return string(hash), nil
 }
 
 func VerifyPassword(password, hashpassword string) bool {
+	if password == "" || hashpassword == "" {
+		return false
+	}
 	err := bcrypt.CompareHashAndPassword([]byte(hashpassword), []byte(password))
 	return err == nil
 }
